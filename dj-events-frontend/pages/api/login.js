80ab06f9140cebc11b4775f4b5f10589678f4b14
Joao Ferreira <joaoferreira.miecom@gmail.com>
2022-08-26 14:57:21 +0300

    [module 7-5] - Store jwt in httponly cookie

--- dj-events-frontend/pages/api/login.js
+++ dj-events-frontend/pages/api/login.js
@@ -1,3 +1,4 @@
+import cookie from "cookie";
 import { API_URL } from "../../config/index";
 
 export default async (req, res) => {
@@ -18,10 +19,20 @@ export default async (req, res) => {
     });
 
     const data = await strapiRes.json();
-    // console.log(data);
+    console.log(data);
 
     if (strapiRes.ok) {
       // @todo - Set cookie
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== "development",
+          maxAge: 60 * 60 * 27 * 7,
+          sameSite: "strict",
+          path: "/",
+        })
+      );
 
       res.status(200).json({ user: data.user });
     } else {
--- dj-events-frontend/pages/events/edit/[id].js
+++ dj-events-frontend/pages/events/edit/[id].js
@@ -152,9 +152,11 @@ export default function EditEventPage({ evt }) {
   );
 }
 
-export async function getServerSideProps({ params: { id } }) {
+export async function getServerSideProps({ params: { id }, req }) {
   const res = await fetch(`${API_URL}/events/${id}?populate=*`);
   const evt = await res.json();
 
+  // ! cookie
+  // console.log(req.headers.cookie);
   return { props: { evt: evt.data } };
 }
