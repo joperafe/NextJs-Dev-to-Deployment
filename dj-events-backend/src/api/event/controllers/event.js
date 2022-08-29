204eccd9870aaf5943be6cdf6216d5b155cfce25
Joao Ferreira <joaoferreira.miecom@gmail.com>
2022-08-30 00:12:14 +0300

    [module 8-4] - Create is owner policy for events

--- dj-events-backend/src/api/event/controllers/event.js
+++ dj-events-backend/src/api/event/controllers/event.js
@@ -9,6 +9,77 @@ const { sanitize } = require("@strapi/utils");
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::event.event", ({ strapi }) => ({
+  // Create event with linked user
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+
+      data.user = ctx.state.user.id;
+      entity = await strapi.query("api::event.event").create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.query("api::event.event").create(ctx.request.body);
+    }
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+
+  // Update user event
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [event] = await strapi.query("api::event.event").findMany({
+      where: { id, user: ctx.state.user.id },
+    });
+
+    if (!event) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+
+      entity = await strapi.db.query("api::event.event").update({
+        where: { id },
+        data,
+        files,
+      });
+    } else {
+      entity = await strapi.db.query("api::event.event").update({
+        where: { id },
+        data: ctx.request.body,
+      });
+    }
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+
+  // Delete user event
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const [events] = await strapi.query("api::event.event").findMany({
+      where: { id, user: ctx.state.user.id },
+    });
+
+    if (!events) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    const entity = await strapi.db.query("api::event.event").delete({ id });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+
   async me(ctx) {
     const user = ctx.state.user;
 
